a3b0dab182695bf99d9cd016c116505da791e2cf
Jonathan Almeida <90427766+JonathanAsf@users.noreply.github.com>
2022-10-14 07:14:45 -0300

    versão 2.0
    O programa está funciona, basta alguns ajustes em relação ao primeiro condicional.
    O nome de algumas variáveis foram mudados para melhor leitura do código, além da adição das partes 6 e 7.

--- calculo.py
+++ calculo.py
@@ -1,68 +1,85 @@
+#parte 0
+
+print('programa para informar quantidade de CO² emitido por sua empresa')
+
 #parte 1
 
-combustivel = int(input('Se sua empresa usa gasolina digite 1, se for diesel digite 2\n'))
-km = int(input('Quantos KM sua empresa rodo no dia? \n'))
-resto = int((km*5)*53)* 0.82 * 0.75 * 3.7 
-resto1 = int((km*5)*53)* 0.82 * 3.7
+combustivel = int(input('se sua empresa usa gasolina digite 1, se for diesel digite 2')) #Precisa criar um condicional para restringir as duas  opções
+km = int(input('quantos KM sua empresa rodo no dia?'))
+gasolina = int((km*5)*53)* 0.82 * 0.75 * 3.7 
+diesel = int((km*5)*53)* 0.82 * 3.7
 if combustivel == 1:
-   print('Sua empresa emitiu:\n',resto,'kg de carbono')
+   print('sua empresa emitiu',gasolina,'KG de carbono')
 elif combustivel == 2:
-   print('Sua empresa emitiu: ',resto1,'kg de carbono')
+   print('sua empresa emitiu',diesel,'KG de carbono')
 else:
-   print('Erro de conexão tente mais tarde ...!')
+   print('erro de conexão tente mais tarde ...!')
+
 
 #parte2
 
+contadeluz =  float(input("Quanto ficou a conta de luz da sua empresa"))
+respostadaluz = float(contadeluz*0.092) 
+porano = int(respostadaluz*(53*5))
+print('por ano sua empresa emiti',porano,'CO²')
+
+
 #parte3
-frete = int(input('Quantos fretes sua empresa faz ao mês? '))
-distanciaMedia = int(input('Qual é a media da distancia do seus clientes? '))
-resto3 = int(frete*distanciaMedia)* 0.82 * 0.75 * 3.7 
-resto4 = (frete*distanciaMedia)* 0.82 * 3.7
+frete = int(input('quantos fretes sua empresa faz ao mes'))
+mediakm = int(input('qual é a media da distancia do seus clientes'))
+gasolina1 = int(frete*mediakm)* 0.82 * 0.75 * 3.7 
+diesel1 = int(frete*mediakm)* 0.82 * 3.7
 if combustivel == 1:
-  print('Sua empresa emitiu',resto3,'KG de carbono ')
+  print('sua empresa emitiu',gasolina1,'KG de CO²')
 elif combustivel == 2:
-  print('Sua empresa emitiu',resto4,'KG de carbono ')
+  print('sua empresa emitiu',diesel1,'KG de CO²')
 else:
-  print('Erro de conexão tente mais tarde ...!'')
+  print('erro de conexão tente mais tarde ...!')
+
 
 #parte4
 
-diminuicao = float(input('Digite quantos KG sua empresa diminuiu nesse ano:\n '))
-distanciaMedia = int(diminuicao / 1000)
-if diminuicao >= 1000:
-  print('Você recebeu:',distanciaMedia,'creditos ')
-elif diminuicao < 1000:
-    print('Você não recebeu nenhum credito ')
+diminuição = float(input('digite quantos KG CO² sua empresa emitiu a menos nesse ano :'))
+respostadimi = int(diminuição / 1000)
+if diminuição >= 1000:
+  print('você recebeu:',respostadimi,'creditos')
+elif diminuição < 1000:
+    print('você não resebeu nenhum credito')
 else:
-    print('Erro de conexão tente mais tarde... ')
-print('digite para qual moeda vc quer converter? ')
+    print('erro de conexão tente mais tarde...')
+
 
 #parte5
-resto5 = int(resto3+resto)
-resto6 = int(resto4+resto1)
-if combustivel == 1:
-   print('Por ano sua empresa gasta',resto5,'KG de CO2 ')
-elif combustivel == 2:
-  print('Por ano sua empresa gasta',resto6,'KG de CO2 ')
 
-#parte 5.1
-if distanciaMedia > 0:
-   print('Compensando vc tera que plantar',distanciaMedia*7,'arvores')
-elif distanciaMedia == 0:
-  print('Compensando foi abaixo de 1 o precissa plantar nenhuma arvores')
+gasolinafinal = int(gasolina1 + gasolina + porano)
+dieselfinal = int(diesel + diesel1 + porano)
+if combustivel == 1:
+   print('Por ano sua empresa gasta',gasolinafinal,'KG de CO²')
+elif combustivel == 2:
+  print('Por ano sua empresa gasta',dieselfinal,'KG de CO²')
 else:
   print('Erro de conexão tente novamente mais tarde...!')
 
 #parte 6
-moeda = int(input('para dolar digite 1,para euro digite 2,para real digite 3?'))
-if moeda == 1:
-  n7 = float(distanciaMedia * 55.30)
-  print('Convertendo para dolar você tera:\n','US$',n7) 
-elif moeda == 2:
-  n7 = float(distanciaMedia * 57)
-  print('Convertendo para euro você tera:\n','Є',n7)
-elif moeda == 3:
-  n7 = float(distanciaMedia * 365)
-  print('Convertendo para real você tera:\n','R$',n7)
+
+if respostadimi > 0:
+   print('Para compensar a taxa de emissão, será necessário plpantar ',respostadimi*7,'árvores')
+elif respostadimi == 0:
+  print('A compensação está equivalente. Não será necessário plantar mais árvores.')
 else:
-  print('Erro de conexão tente mais tarde...\n')
+  print('erro de conexão tente novamente mais tarde...!')
+
+#parte 7
+print("Você possui crédito, escolha a moeda que deseja obter a cotação")
+conversão = int(input('Digite 1 para dolar, Digite 2 para euro, Digite 3 para real'))
+if conversão == 1:
+  n7 = float(respostadimi * 55.30)
+  print('Convertendo para dolar você tera:',n7) 
+elif conversão == 2:
+  n7 = float(respostadimi * 57)
+  print('Convertendo para euro você tera:',n7)
+elif conversão == 3:
+  n7 = float(respostadimi * 365)
+  print('Convertendo para real você tera:',n7)
+else:
+  print('erro de conexão tente mais tarde...')
