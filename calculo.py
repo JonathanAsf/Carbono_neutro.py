f86af4ef098569c6ab5f6960748de5c9340c74ec
Jonathan Almeida <90427766+JonathanAsf@users.noreply.github.com>
2022-10-31 04:52:48 -0300

    Alpha
    Algumas partes do código anterior foram removidas e outras foram alteradas como;
    Ao final do programa é exibido o crédito em todas as moedas populares;
    O cálculo de crédito precisa ser reformulado (80);
    A opção de álcool como combustível foi adicionada.

--- calculo.py
+++ calculo.py
@@ -1,101 +1,132 @@
 #Apresentação
 
-print('Descubra se sua empresa possui crédito resultante de baixa emissão de CO²')
+print('\n Descubra quanto de CO² sua empresa emite anualmente \n')
 
 #Cálculo de emissão
 
 print('Escolha o combustível que sua empresa utiliza:')
-print('Gasolina: Digite 1') 
+print('Gasolina: Digite 1')
 print('Diesel: Digite 2')
-combustivel = int(input()) 
+print('Alcool: Digite 3')
+combustivel = int(input())
 
 #Caso outro valor seja inserido, esse condicional fechará o programa
 
-if combustivel != 1 and combustivel != 2:
+if combustivel != 1 and combustivel != 2 and combustivel !=3:
   print('Opção indisponível, tente novamente.')
   quit()
 
 #Fórmula para o cálculo e apresentação do resultado
 
 km = int(input('Quantos KM sua empresa roda por dia?\n'))
-gasolina = int(((km*5)*53)* 0.82 * 0.75 * 3.7) #Fórmula para CO² em Kg emitido usando gasolina
+gasolina = int(((km*5)*53)* 0.82 * 0.75 * 3.7) #Fórmula para CO² em Kg emitido uusando gasolina
 diesel = int(((km*5)*53)* 0.82 * 3.7) #Fórmula para CO² em Kg emitido usando diesel
+alcool = int(((km*5)*53)*2.716) #Fórmula para Co² em Kg emitido usando etanol
 
 if combustivel == 1:
-   print(f'Sua empresa emitiu {gasolina:.1f}KG de carbono')
-else:
-   print(f'Sua empresa emitiu {diesel:.1f} KG de carbono')
-
-#Cálculo de gasto apenas utilizando energia elétrica
-
-contadeluz =  float(input("Qual é o valor médio da conta de luz da sua empresa?\n"))
-respostadaluz = float(contadeluz*0.092) 
-porano = int(respostadaluz*(53*5))
-print(f'Sua empresa emite {porano:.1f}CO² anualmente')
+   print(f'Sua empresa emitiu por ano {gasolina:.2f} KG de carbono')
+elif combustivel == 2:
+   print(f'Sua empresa emitiu por ano {alcool:.2f} KG de carbono')
+elif combustivel == 3:
+    print(f'Sua empresa emitiu por ano {diesel:.2f} KG de carbono')
 
 #Cálculo baseado distancia percorrida
 
 frete = int(input('Quantos fretes sua empresa faz ao mês?\n'))
-mediakm = int(input('Qual é a média da distância do seus clientes?\n'))
-gasolina1 = int((frete*mediakm)* 0.82 * 0.75 * 3.7) 
-diesel1 = int((frete*mediakm)* 0.82 * 3.7)
+mediakm = int(input('Qual é a média da distância(km) do seus clientes?\n'))
+distgaso = int((frete*mediakm)* 0.82 * 0.75 * 3.7)
+distdisel = int((frete*mediakm)* 0.82 * 3.7)
+distalco = int((frete*mediakm)*2.716)
 
 if combustivel == 1:
-  print(f'Sua empresa emitiu: {gasolina1:.1f}KG de CO²')
-elif combustivel == 2:
-  print(f'Sua empresa emitiu: {diesel1:.1f}KG de CO²')
+  print(f'Sua empresa emite:{distgaso:.2f} KG de CO² por mês')
+elif distgaso == 2:
+  print(f'Sua empresa emite:{distdisel:.2f} KG de CO² por mês')
+elif combustivel == 3:
+  print(f'Sua empresa emite:{distalco:.2f} KG de CO² por mês')
 else:
   print('Erro de conexão tente mais tarde ...!')
 
-#Avaliação do crédito
-
-diminuição = float(input('Quantos KG CO² sua empresa emitiu a menos nesse ano? \n'))
-respostadimi = int(diminuição / 1000)
-if diminuição >= 1000:
-  print(f'Você recebeu: {respostadimi:.1f} créditos\n')
-elif diminuição < 1000:
-    print('Você não possui crédito disponível\n')
-else:
-    print('Erro de conexão tente mais tarde...')
-
-
 #Cálculo de uso de combustível total
-
-gasolinafinal = int(gasolina1 + gasolina + porano) #Fórmula para gasolina gasta ao ano
-dieselfinal = int(diesel + diesel1 + porano) #Fórmula para diesel gasto ao ano
+gasolinafinal = int(distgaso) #Fórmula para gasolina gasta ao ano
+dieselfinal = int(diesel + distdisel) #Fórmula para diesel gasto ao ano
+alcoolfinal = int(alcool + distalco) #Fórmula para alcool gasto ao ano
+ 
 if combustivel == 1:
-   print(f'Por ano sua empresa gasta: {gasolinafinal}KG de CO²\n')
+   print(f'Sua empresa emite: {gasolinafinal} KG de CO² por ano\n')
 elif combustivel == 2:
-  print(f'Por ano sua empresa gasta: {dieselfinal}KG de CO²\n')
+   print(f'Sua empresa emite: {dieselfinal} KG de CO² por ano\n')
+elif combustivel == 3:
+   print(f'Sua empresa emite: {alcoolfinal} KG de CO² por ano\n')
 else:
-  print('Erro de conexão tente novamente mais tarde...!')
+   print('Erro de conexão tente novamente mais tarde...!')
 
-#Diminuição de Co²
 
-if respostadimi > 0:
-   print(f'Para compensar a taxa de emissão de CO², será necessário plantar {respostadimi*7} árvores\n')
-elif respostadimi == 0:
-  print('A compensação está equivalente. Não será necessário plantar mais árvores.\n')
-else:
-  print('Erro de conexão tente novamente mais tarde...!')
-
-#Conversão papa moedas
-
-print("Escolha uma moeda para converter o crédito existente")
-print('Dólar: Digite 1') 
-print('Euro: Digite 2')
-print('Real: Digite 3')
-
-conversão = int(input(''))
-
-if conversão == 1:
-  n7 = float(respostadimi * 55.30)
-  print(f'Convertendo para dolar você terá: US${n7:.2f} disponíveis em crédito') 
-elif conversão == 2:
-  n7 = float(respostadimi * 57)
-  print(f'Convertendo para euro você terá: €{n7:.2f} disponíveis em crédito')
-elif conversão == 3:
-  n7 = float(respostadimi * 365)
-  print(f'Convertendo para real você terá: R${n7:.2f} disponíveis em crédito')
+#Avaliação do crédito
+#Necessário reformular essa parte do código
+if combustivel == 1:
+  emgasol = int(gasolinafinal/1000) #Emissão de carbono utilizando gasolina
+  print(f'')
+elif combustivel == 2:
+  emdie = int(dieselfinal/1000) #Emissão de carbono utilizando diesel
+  print(f'')
+elif combustivel == 3:
+  emalco = int(alcoolfinal/1000) #Emissão de carbono utilizando alcool
+  print(f'')
 else:
   print('Erro de conexão tente mais tarde...')
+
+#Constatação da equivalência 
+#Caso não haja equivalência, será feito um cálculo visando alcança-la
+
+if combustivel ==1:
+    print('Para compensar a taxa de emissão de CO², será necessário plantar ',emgasol,'árvores\n')
+elif combustivel ==2:
+    print('Para compensar a taxa de emissão de CO², será necessário plantar ',emdie*7,'árvores\n')
+elif combustivel ==3:
+    print('Para compensar a taxa de emissão de CO², será necessário plantar ',emalco*7,'árvores\n')
+
+elif combustivel ==1:
+    print('A compensação está equivalente. Não será necessário plantar mais árvores.\n')
+elif combustivel ==2:
+    print('A compensação está equivalente. Não será necessário plantar mais árvores.\n')
+elif combustivel ==3:
+    print('A compensação está equivalente. Não será necessário plantar mais árvores.\n')
+else:
+  print('Erro de conexão tente novamente mais tarde...!')
+
+#Conversão para moedas
+
+#Gasolina
+if  combustivel == 1:
+    credito = float(emgasol * 55.30)
+    print('Convertendo para dolar você terá: U$',credito)
+
+    credito = float(emgasol * 57)
+    print('Convertendo para euro você terá:',credito)
+
+elif combustivel == 1:
+    credito = float(emgasol* 365)
+    print('Convertendo para real você terá:',credito)
+
+#Diesel
+elif combustivel == 2:
+    credito = float(emdie * 55.30)
+    print('Convertendo para dolar você terá: U$',credito)
+
+    credito = float(emdie * 57)
+    print('Convertendo para euro você terá: U$',credito)
+
+    credito = float(emdie* 365)
+    print('Convertendo para real você terá: U$',credito)
+
+#Álcool
+elif combustivel == 3:
+    credito = float(emalco*55.30)
+    print('Convertendo para dolar você terá: U$', credito)
+
+    credito = float(emalco * 57)
+    print('Convertendo para euro você terá: ', credito)
+
+    credito = float(emalco * 365)
+    print('Convertendo para real você terá:',credito)
